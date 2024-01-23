fe3a4c75d19fa1e9ed04c72d1e081b61b7ac7713
Danil-SW-1 <danil@drawer.ai>
2024-01-23 20:13:27 +0200

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+mod api;
+mod models;
+mod repository;
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Response {
+    pub message: String,
+}
+
+#[get("/health")]
+async fn healthcheck() -> impl Responder {
+    let response = Response {
+        message: "Deromen has a big cock".to_string(),
+    };
+
+    HttpResponse::Ok().json(response)
+}
+
+async fn not_found() -> Result<HttpResponse> {
+    let response = Response {
+        message: "Resource not found".to_string(),
+    };
+    Ok(HttpResponse::NotFound().json(response))
+}
+
+// #[tokio::main]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(healthcheck)
+            .default_service(web::route().to(not_found))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
